74a10fa55f063fc8151ff3533c97ca0627d66de9
Alibekov Sultan <smago-13579@oma.kz>
2024-05-26 11:58:17 +0500

    scroll animation

---
+++ scroll_animation/script.js
@@ -0,0 +1,23 @@
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes();
+console.log('window height: ' + window.innerHeight);
+
+function checkBoxes() {
+
+    const triggerBottom = window.innerHeight / 5 * 4;
+
+    console.log('triggerBottom: ' + triggerBottom);
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+
+        if (boxTop < triggerBottom) {
+            box.classList.add('show');
+        } else {
+            box.classList.remove('show');
+        }
+    });
+}
