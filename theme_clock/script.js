5bd7a5ae200574686a881d82bec60c92d2fcea6c
Alibekov Sultan <smago-13579@oma.kz>
2024-06-28 17:11:40 +0500

    theme clock

---
+++ theme_clock/script.js
@@ -0,0 +1,51 @@
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const secondEl = document.querySelector('.second');
+const timeEl = document.querySelector('.time');
+const dateEl = document.querySelector('.date');
+const toggle = document.querySelector('.toggle');
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+
+toggle.addEventListener('click', () => {
+    const html = document.querySelector('html');
+
+    if (toggle.innerHTML === 'Dark mode') {
+        toggle.innerHTML = 'Light mode';
+    } else {
+        toggle.innerHTML = 'Dark mode';
+    }
+    html.classList.toggle('dark');
+});
+
+function setTime() {
+    const time = new Date();
+    const month = time.getMonth();
+    const day = time.getDay();
+    const date = time.getDate();
+    const hours = time.getHours();
+    const hoursForClock = hours % 12;
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    const ampm = hours > 11 ? 'PM' : 'AM';
+
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)}deg)`;
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`;
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`;
+
+    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`;
+    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
+
+    const needleSecond = document.querySelector(".needle.second");
+    needleSecond.style.transition = `${seconds === 0 ? "none" : "all 0.5s ease-in"}`;
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+};
+
+setTime();
+
+setInterval(setTime, 1000);
